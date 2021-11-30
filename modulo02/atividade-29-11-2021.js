79f6f2b7f07cf4098e51e35c68974dd797da9786
Well-lleW <wellew_@outlook.com>
2021-11-30 03:51:07 -0300

    atividade 29/11/2021

---
+++ modulo02/atividade-29-11-2021.js
@@ -0,0 +1,50 @@
+let phrase = "I didn’t do it! Nobody saw me do it. You can’t prove anything!";
+let phraseNumber = document.getElementById("phraseInput").value;
+let p;
+let phraseCount = 0;
+let phraseAmount = 0;
+let wipeCount = 0;
+
+
+function wipe() {
+  document.getElementById("whiteBoard").innerHTML = "";
+  phraseAmount = 0;
+  phraseCount = 0;
+  wipeCount = 0;
+}
+function createPhrase() {
+  p = document.createElement("p");
+  p.innerHTML = phrase;
+  document.getElementById("whiteBoard").appendChild(p);
+  function typeWriter(element) {
+    const textArray = phrase.split("");
+    element.innerHTML = "";
+    textArray.forEach((letter, i) => {
+      setTimeout(() => (element.innerHTML += letter), 110 * i);
+    });
+  }
+  typeWriter(p);
+}
+
+function newPhrase() {
+  wipe();
+  let phraseNumber = document.getElementById("phraseInput").value;
+  
+  while (phraseAmount < phraseNumber) {
+    phraseAmount += 1;
+    if (phraseCount < 11) {
+      phraseCount += 1;
+      createPhrase();
+    } else if (phraseCount < 11) {
+      phraseCount += 1;
+      createPhrase();
+    } else {
+      wipeCount += 1;
+      document.getElementById("whiteBoard").innerHTML = "";
+      createPhrase();
+      phraseCount = 0;
+      document.getElementById("notes").innerHTML =
+        "Wiped " + wipeCount + "<br/>" + "Remainder " + (phraseNumber % 11);
+    }
+  }
+}

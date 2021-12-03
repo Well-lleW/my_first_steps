3a49bc798f97a3e7e3e7c6a503334915d6d1828f
Well-lleW <wellew_@outlook.com>
2021-12-03 11:50:31 -0300

    -consolelog

--- modulo02/atividade-02-12-2021.js
+++ modulo02/atividade-02-12-2021.js
@@ -22,7 +22,6 @@ function playerMove(mv1, mv2) {
 }
 
 function winner(player) {
-  console.log(player);
   if ((board[0][0] === player && board[0][1] === player && board[0][2] === player) || 
     (board[1][0] === player && board[1][1] === player && board[1][2] === player) || 
     (board[2][0] === player && board[2][1] === player && board[2][2] === player) || 

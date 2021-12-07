eb42d24ba29da5d1e2388cc0e84110a5ed891e46
Well-lleW <wellew_@outlook.com>
2021-12-07 10:50:25 -0300

    f5

--- modulo02/atividade-05-12-2021.js
+++ modulo02/atividade-05-12-2021.js
@@ -4,6 +4,33 @@ let win1 = 0,
 
 let laps;
 
+const popular = {
+  maxMin: 180,
+  maxMax: 200,
+  minMin: 110,
+  minMax: 130,
+  skidMin: 0.03,
+  skidMAx: 0.04,
+};
+
+const sport = {
+  maxMin: 195,
+  maxMax: 215,
+  minMin: 125,
+  minMax: 145,
+  skidMin: 0.02,
+  skidMAx: 0.03,
+};
+
+const superSport = {
+  maxMin: 210,
+  maxMax: 230,
+  minMin: 140,
+  minMax: 160,
+  skidMin: 0.01,
+  skidMAx: 0.0175,
+};
+
 const ednaCar = {
   name: "Edna",
   model: "",
@@ -25,30 +52,34 @@ const jucaCar = {
   skid: "",
 };
 
-document.getElementById("test").innerHTML = ednaCar.model;
-
 function sortCar(driver) {
   d = Math.random() * 100;
   if (d < 61) {
-    min = Math.random() * (130 - 110 + 1) + 110;
-    max = Math.random() * (200 - 180 + 1) + 180;
+    min =
+      Math.random() * (popular.minMax - popular.minMin + 1) + popular.minMin;
+    max =
+      Math.random() * (popular.maxMax - popular.maxMin + 1) + popular.maxMin;
     driver.speed = Math.random() * (max - min) + min;
     driver.model = 1;
-    driver.skid = sortSkid(0.04, 0.03);
+    driver.skid = sortSkid(popular.skidMAx, popular.skidMin);
     d = 0;
-  }else if (d < 96) {
-    min = Math.random() * (145 - 125 + 1) + 125;
-    max = Math.random() * (215 - 195 + 1) + 195;
+  } else if (d < 96) {
+    min = Math.random() * (sport.minMax - sport.minMin + 1) + sport.minMin;
+    max = Math.random() * (sport.maxMax - sport.maxMin + 1) + sport.maxMin;
     driver.speed = Math.random() * (max - min) + min;
     driver.model = 2;
-    driver.skid = sortSkid(0.03, 0.02);
+    driver.skid = sortSkid(sport.skidMAx, sport.skidMin);
     d = 0;
-  }else {
-    min = Math.random() * (160 - 140 + 1) + 140;
-    max = Math.random() * (230 - 210 + 1) + 210;
+  } else {
+    min =
+      Math.random() * (superSport.minMax - superSport.minMin + 1) +
+      superSport.minMin;
+    max =
+      Math.random() * (superSport.maxMax - superSport.maxMin + 1) +
+      superSport.maxMin;
     driver.speed = Math.random() * (max - min) + min;
     driver.model = 3;
-    driver.skid = sortSkid(0.0175, 0.01);
+    driver.skid = sortSkid(superSport.skidMAx, popular.skidMin);
     d = 0;
   }
 }
@@ -66,12 +97,9 @@ function startRace() {
     console.log(ednaCar);
     console.log(pedroCar);
     console.log(jucaCar);
-    speed1 = ednaCar.speed * (1-ednaCar.skid);
-    // console.log(speed1);
-    speed2 = pedroCar.speed * (1-pedroCar.skid);
-    // console.log(speed2);
-    speed3 = jucaCar.speed * (1-jucaCar.skid);
-    // console.log(speed3);
+    speed1 = ednaCar.speed * (1 - ednaCar.skid);
+    speed2 = pedroCar.speed * (1 - pedroCar.skid);
+    speed3 = jucaCar.speed * (1 - jucaCar.skid);
 
     if (speed1 > speed2 && speed1 > speed3) {
       win1 += 1;
@@ -91,6 +119,42 @@ function startRace() {
     document.getElementById("leaderboard").innerHTML =
       jucaCar.name + " ganhou " + win3 + " das " + laps + " voltas!";
   }
+  document.getElementById("table").innerHTML =
+    "</br>" + ednaCar.name + " ganhou " + win1;
+  document.getElementById("table").innerHTML +=
+    "</br>" + pedroCar.name + " ganhou " + win2;
+  document.getElementById("table").innerHTML +=
+    "</br>" + jucaCar.name + " ganhou " + win3;
+
+  let raceHistory = "<tr>";
+  raceHistory += "<td>" + ednaCar.name + "</td>";
+  // raceHistory += "<td>" + parseInt(ednaCar.speed) + "</td>";
+  // raceHistory += "<td>" + parseFloat(ednaCar.skid).toFixed(3) + "</td>";
+  raceHistory += "<td>" + win1 + "</td>";
+  raceHistory += "</tr>";
+  document.getElementById("table").innerHTML = raceHistory;
+
+  raceHistory = "";
+
+  raceHistory = "<tr>";
+  raceHistory += "<td>" + pedroCar.name + "</td>";
+  // raceHistory += "<td>" + parseInt(pedroCar.speed) + "</td>";
+  // raceHistory += "<td>" + parseFloat(pedroCar.skid).toFixed(3) + "</td>";
+  raceHistory += "<td>" + win2 + "</td>";
+  raceHistory += "</tr>";
+  document.getElementById("table").innerHTML += raceHistory;
+
+  raceHistory = "";
+
+  raceHistory = "<tr>";
+  raceHistory += "<td>" + jucaCar.name + "</td>";
+  // raceHistory += "<td>" + parseInt(jucaCar.speed) + "</td>";
+  // raceHistory += "<td>" + parseFloat(jucaCar.skid).toFixed(3) + "</td>";
+  raceHistory += "<td>" + win3 + "</td>";
+  raceHistory += "</tr>";
+  document.getElementById("table").innerHTML += raceHistory;
+  document.getElementById("bigTable").style.display = 'block';
+
   clean();
 }
 

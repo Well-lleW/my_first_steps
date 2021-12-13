44205fb2efc3c79e7c232e98daf89263d83f410d
Well-lleW <wellew_@outlook.com>
2021-12-13 09:46:49 -0300

    f5

--- modulo02/atividade-12-12-2021.js
+++ modulo02/atividade-12-12-2021.js
@@ -14,12 +14,12 @@ function addClient() {
   dueDay = new Date(document.querySelector('input[type="date"]').value);
   clientDueDay = document.querySelector('input[type="date"]').value;
   days =
-    Math.floor((today.getTime() - dueDay.getTime()) / (3600 * 1000 * 24)) - 1;
+    Math.floor((today.getTime() - dueDay.getTime()) / (3600 * 1000 * 24));
   clientName = document.querySelector('input[type="text"]').value;
   clientOwed = parseFloat(
     document.querySelector('input[type="number"]').value
   ).toFixed(2);
-  clientTax = 2 + days * 0.1;
+  clientTax = days * 0.1;
   
   clients.name[i] = clientName;
   clients.dueDate[i] = clientDueDay;
@@ -30,7 +30,7 @@ function addClient() {
   clientTable += "<td>" + clients.name[i] + "</td>";
   clientTable += "<td>" + clients.dueDate[i] + "</td>";
   clientTable += "<td>" + clients.owed[i] + "</td>";
-  clientTable += "<td>" + clients.tax[i] + "</td>";
+  clientTable += "<td>" + (2 + clients.tax[i]) + "</td>";
   clientTable += "<td>" + "-" + "</td>";
   clientTable += "</tr>";
   document.getElementById("table").innerHTML += clientTable;
@@ -43,15 +43,17 @@ function addClient() {
 
 function calculate() {
   document.getElementById("table").innerHTML = "";
+
+  clients.total = clients.owed.map(x => x * 1.02);
+
   for (let i = 0; i < clients.name.length; i++) {
-    clients.total[i] = (clients.owed[i] * (1 + clients.tax[i] / 100)).toFixed(
-      2
-    );
+  
+    clients.total[i] = (clients.total[i] * (1 + clients.tax[i] / 100)).toFixed(2);
     let clientTable = "<tr>";
     clientTable += "<td>" + clients.name[i] + "</td>";
     clientTable += "<td>" + clients.dueDate[i] + "</td>";
     clientTable += "<td>" + clients.owed[i] + "</td>";
-    clientTable += "<td>" + clients.tax[i] + "</td>";
+    clientTable += "<td>" + (2 + clients.tax[i]) + "</td>";
     clientTable += "<td>" + clients.total[i] + "</td>";
     clientTable += "</tr>";
     document.getElementById("table").innerHTML += clientTable;
